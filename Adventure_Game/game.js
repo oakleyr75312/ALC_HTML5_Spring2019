46c29d1aee3490afb593741b37006646d22c12db
OakleyRoberts <oakley.r929@stu.neob.edu>
2019-02-05 10:29:01 -0700

    broke everything

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -11,6 +11,32 @@ Multiline comment
 
 // prompt("What type of pokemon do you like?");
 
+//function Blacksmith(){
+            //alert("");
+            
+            //var blacksmith = prompt("What do you want to buy? /n - sword");
+                //if(blacksmith == "sword" && inventory.coin >= 100){
+                    //var swordBuy = confirm("You sure");
+                        //if(swordBuy){
+                            //inventory.sword ++;
+                            //alert("You own " +inventory.sword+" swords")
+                            //inventory.coin = inventory.coin - 100;
+                            //inventory.coin -= 100;
+                            //alert("You have "+inventory.coins+" coins remaining");
+                            //Blacksmith();
+                        //}
+
+var inventory = {
+    coin:1000,
+    bread:0,
+    sword:0,
+    map:0,
+}
+
+
+
+
+
 Game();
 
 function Game(){
@@ -26,9 +52,12 @@ function Game(){
         
         if(boat == "look at the island" || boat == "island"){
             
-            var IslandLook = alert("You see a small island with only 3 trees to the north. The sand is dry.You see a small box poking out of the sand. \n -look in water \n -look in the boat.")
+            var IslandLook = prompt("You see a small island with only 3 trees to the north. You see a small box poking out of the sand. \n -look in the water \n -look in the boat");
         }
         
+        
+        
+    
         else if(boat == "look in the water" || boat == "water"){
             // Fall asleep because of the water
             alert("You look over at the water and while you watch the waves you start to fall asleep.");
@@ -42,56 +71,95 @@ function Game(){
                 alert("Game over!");
             }
         }
+    }
+        
+        
+        
         else if(boat == "look in the boat" || boat == "boat"){
             
-            var BoatLook = prompt("You see a cloak under the seat of the boat. When you look at it. It seems to look back. You feel it's fabric and notice it feels weird. \n -Pick it up \n -Throw it in the water \n -leave it in the boat.").toLowerCase();
+            var BoatLook = prompt("You see a cloak under the seat of the boat. When you look at it. It seems to look back. You feel it's fabric it feels like metal but it acts like fabric. \n -pick it up \n -throw it in the water \n -leave it in the boat.").toLowerCase();
             
             if(boat == "pick it up" || "keep"){
-                alert("YOU GOT A CLOAK!")
+                alert("YOU GOT A CLOAK!");
             }
             
             else if(boat == "throw it in the water" || "water"){
-                alert("The cloak hits the water and explodes throwing you and your boat into a tree.")
+                alert("The cloak hits the water and explodes throwing you and your boat into a tree.");
             }
             else if(boat == "leave it in the boat" || "leave it"){
-                alert("You left it in the boat.")
+                alert("You left it in the boat.");
             }
         }
         
-        else{
-            alert("I don't know what "+boat+" is!");
-            boat();
+        
+               
+        
+        
+        
+            else{
+                alert("I don't know what "+boat+" is!");
+                boat();
         }
-    }
     
     function Water(){
         
         var water1 = prompt("You enter a dank swamp. \n -swim \n -follow path").toLowerCase();
         
         if(water1 == "swim" || water1 == "go swimming"){
-            alert("You get eaten by a swamp monster...Game Over!")
+            alert("You get eaten by a swamp monster...Game Over!");
         
         }
         
         else if(water1 == "follow path" || water1 == "follow"){
-            var waterpath = prompt(" -eat lunch \n -go north")
+            var waterpath = prompt(" -eat lunch \n -go north");
             // Eat lunch event
             if (waterPath == "eat lunch"){
-                alert("You eat your lunch, it was poisioned...how sad! Game Over!")
+                alert("You eat your lunch, it was poisioned...how sad! Game Over!");
             }
             // Go north to castle event
             else if(waterPath == "noth"){
                 alert("You travel north");
-                Castle();
+                Water();
             }
         }
         
         else{
-            alert("I don't understand " + Water1)
-            Water();
+            alert("I don't understand " + water1)
+            water().toLowerCase();
             
         }
+    
+    
     }
     
     
+        
+        function Castle(){
+            var insideCastle = prompt("- upstairs \n - downstairs /n - courtyard /n - balcony /n look").toLowerCase();
+            
+            switch(insideCastle){
+                case "upstairs":
+                    var upstairs = prompt("You walk a long flight of stairs to the top floor of the castle.");
+                    
+                    Castle();
+                break;
+                case "downstairs":
+                    prompt("You go downstairs");
+                
+                break;
+                case "courtyard":
+                    prompt("You go courtyard");
+                
+                break;
+                case "balcony":
+                    prompt("You go balcony");
+                
+                break;
+                default:
+                    alert("I don't know what "+insideCastle+" is!");
+                break;
+              
+            }
+        }
 }
+ 
